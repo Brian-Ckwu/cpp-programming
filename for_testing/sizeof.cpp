8576132f4d0bb626b8c9c60394c65ceec099ba68
Brian Wu <sonny40920710@gmail.com>
2021-06-16 22:55:46 +0800

    Add sizeof.cpp: check the bytes of common types

---
+++ for_testing/sizeof.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Sizes of common types:\n";
+    printf("bool: %zu\n", sizeof(bool));
+    printf("bool: %zu\n", sizeof(char));
+    printf("bool: %zu\n", sizeof(int));
+    printf("bool: %zu\n", sizeof(double));
+
+    return 0;
+}

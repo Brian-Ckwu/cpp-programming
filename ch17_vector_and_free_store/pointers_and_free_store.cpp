ff1849ae6a297573879fa35a3c1ee45d410dd4fb
Brian Wu <sonny40920710@gmail.com>
2021-06-29 17:31:39 +0800

    Add pointers_and_free_store.cpp

---
+++ ch17_vector_and_free_store/pointers_and_free_store.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double* ptr = new double[4]; // allocate sizeof(double) * 4 bytes on the free store (heap) & return the pointer to the first double
+
+  // char* cptr = new double; // error: double* cannot be assigned to char*
+
+  double a1 = *ptr; // assign the value of the first object pointed by ptr to "another" area of memory named a
+  double b1 = ptr[2]; // assign the value of the third object pointed by ptr to "another" area of memory named b
+  double& a2 = *ptr; // reference to the area of memory ptr pointed to
+  double& b2 = ptr[2];
+
+  cout << a1 << " " << b1 << endl; // what would be the value? random?
+  cout << a2 << " " << b2 << endl; // what would be the value? random?
+
+  *ptr = 10;
+  ptr[2] = 15;
+
+  cout << a1 << " " << b1 << endl;
+  cout << a2 << " " << b2 << endl;
+
+  a2 = 11;
+  b1 = 11;
+
+  cout << *ptr << " " << ptr[2] << endl;
+
+  return 0;
+}
